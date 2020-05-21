c764e126272fb0fb371dee0a03c0753a0bae2a27
Ve <vgonzalezgab@gmail.com>
2020-05-21 15:43:51 +0100

    created input manager

---
+++ files/inputManager.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace vgg {
+
+class InputManager {
+
+public:
+    InputManager( ) { }
+    ~InputManager( ) { }
+    
+    bool IsSpriteClicked( sf::Sprite object, sf::Mouse::Button button, sf::RenderWindow &window );
+    
+    sf::Vector2i GetMousePosition( sf::RenderWindow &window );
+    
+};
+
+}
+
+
