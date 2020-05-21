e3410d69c9951e5df81aacabda14e600991c3b07
Ve <vgonzalezgab@gmail.com>
2020-05-21 16:38:39 +0100

    added splash state

--- files/main.cpp
+++ files/main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include "game.hpp"
+#include "DEFINITIONS.hpp"
 
 int main( ) {
+    
+    vgg::Game( SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird" );
     
     return EXIT_SUCCESS;
     
