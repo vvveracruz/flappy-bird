37391b94f5f9978df0ace9c387001bf361d13f5b
Ve <vgonzalezgab@gmail.com>
2020-05-21 12:28:12 +0100

    created heather files (i think??)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #  Flappy bird
 
+Src: [Youtube tutorial playlist](https://www.youtube.com/playlist?list=PLRtjMdoYXLf7DB3--POF9lYzaZu0poT5V).
--- files/main.cpp
+++ files/main.cpp
@@ -1,35 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main( ) {
     
-    sf::RenderWindow window( sf::VideoMode(  640, 480 ), "Hello" );
-    
-    while ( window.isOpen() ) {
-        
-        sf::Event event;
-        
-        // handle events
-        while ( window.pollEvent( event ) ) {
-            switch ( event.type ) {
-                case sf::Event::Closed:
-                    window.close();
-                    
-                    break;
-                    
-                default:
-                    break;
-            }
-        }
-        
-        // update game
-        
-        window.clear( );
-        
-        // draw objects here
-        
-        window.display( );
-        
-        
-    }
+    return EXIT_SUCCESS;
     
 }
